35c5d7791b23a200cbce20b741cd9baf5f96e2f0
Sarah Elzinga <sevans924@gmail.com>
2019-05-14 16:52:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// const writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(names, event) {
+  let array = []
+  for (let i = 0; i < names.length; i++) {
+   array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+ }
+
+ return array;
+}
+
+
+function countdown(num) {
+  let countdown = num;
+  while (countdown >= 0) {
+    console.log(countdown--);
+}
+}
