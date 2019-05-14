0a35db10ea185e93d31416135b638bad8e7d5a9f
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-05-14 17:14:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function countdown() {
+
+let countdown = 10
+
+while (countdown >= 0) {
+  console.log(countdown)
+  countdown--
+}
+};
+
+
+
+function writeCards(namesArray, event) {
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouCards
+}
+
+// writeCards(strName);
