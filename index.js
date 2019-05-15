dc3da37265a481ef0d34a681d4fb7187749c93b4
Sonia Dumitru <soni.dumitru@gmail.com>
2019-05-14 21:52:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+  let a = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    a.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  };
+  return a;
+};
+
+function countdown(integer) {
+   do {
+    console.log(integer);
+    integer--;
+  } while (integer >= 0);
+};
